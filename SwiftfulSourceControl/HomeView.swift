87c5261b3f7a5001821af0c215ddd4aa533fc2a1
hexiezhe <hexiezhe@yahoo.com>
2026-05-04 01:47:48 +0700

    add home view

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by woei chen on 04/05/2026.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2")
+    }
+}
+
+#Preview {
+    HomeView()
+}
