72515dc1ec3e6a899d3c957c7da01f046a1face8
hexiezhe <hexiezhe@yahoo.com>
2026-05-03 14:09:45 +0700

    My first commit

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -11,9 +11,10 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
+                .font(.largeTitle)
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Swiftful Thinking Bootcamp - GitControl Clone Version Testing")
         }
         .padding()
     }
