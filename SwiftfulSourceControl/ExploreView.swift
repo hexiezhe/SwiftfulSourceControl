69fd4192fa6b4c3982045d7db6482eae985867ce
hexiezhe <hexiezhe@yahoo.com>
2026-05-04 15:33:09 +0700

    [Feature] Add explore view

---
+++ SwiftfulSourceControl/ExploreView.swift
@@ -0,0 +1,19 @@
+//
+//  ExploreView.swift
+//  SwiftfulSourceControl
+//
+//  Created by woei chen on 04/05/2026.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExploreView()
+}
